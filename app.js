51a479c54e2d6ea1b060176c22341e61a296413e
Jake Samuels <samuels242@googlemail.com>
2023-03-26 13:07:36 +0100

    app.use(express.json())

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const emotionsRouter = require("./routes/emotionsRoutes");
 
 const app = express();
 
+app.use(express.json());
+
 app.use("/api/v1/emotions", emotionsRouter);
 
 module.exports = app;
