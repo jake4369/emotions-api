af0cebf32df3adc597908b933b2fceb8e74e7f67
Jake Samuels <samuels242@googlemail.com>
2023-03-28 12:05:57 +0100

    Enable cors

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cors = require(cors);
 
 const emotionsRouter = require("./routes/emotionsRoutes");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/v1/emotions", emotionsRouter);
